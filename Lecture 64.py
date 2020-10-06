3b6a1f2b1d8d0a50794ff45db34fcf4307284d13
bermby38 <30828158+bermby38@users.noreply.github.com>
2020-10-06 23:10:16 +0700

    Create Lecture 64.py

---
+++ Lecture 64.py
@@ -0,0 +1,11 @@
+myFriends = ['A','B','C']
+print(1,myFriends)
+print(myFriends[2])
+
+myFriends.append('D')
+print(2,myFriends)
+print(myFriends[2])
+
+myFriends.remove('A')
+print(3,myFriends)
+print(myFriends[2])
