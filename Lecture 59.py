0b3570e94e56f0daeba3f5b3a8a15387d4c7350d
bermby38 <bermby38@hotmail.com>
2020-10-06 17:32:20 +0700

    Update Lecture 59.py

---
+++ Lecture 59.py
@@ -0,0 +1,13 @@
+"""
+inputName = input("Name : ")
+inputLastname = input("Lastname : ")
+print("Hello %s %s" %(inputName.capitalize(), inputLastname.capitalize()))
+
+print("Welcome".center(30,"*"))
+print("Well".center(30,"*"))
+print("HelloTestTestTestTestTest".center(10,"*"))
+
+"""
+inputName = input("Name : ")
+textOutput = "Hello %s"%(inputName)
+print(textOutput.capitalize().center(20,"-"))
