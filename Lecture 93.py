f0bcd3a349549818df8c3ef2406fbee165d7b41f
bermby38 <bermby38@hotmail.com>
2020-10-09 15:38:37 +0700

    Update Lecture 93.py

--- Lecture 93.py
+++ Lecture 93.py
@@ -3,7 +3,12 @@ from tkinter import *
 def sayHelloWorld():
    print("Hello World")
 
-MainWindow = Tk()
-button = Button(MainWindow, text="Click me", command=sayHelloWorld)
+MainWindow1 = Tk()
+button = Button(MainWindow1, text="Click me", command=sayHelloWorld)
 button.place(x=50, y=20)
-MainWindow.mainloop()
+MainWindow1.mainloop()
+
+MainWindow2 = Tk()
+button2 = Button(MainWindow2, text="Click me", command=sayHelloWorld)
+button2.place(x=50, y=20)
+MainWindow2.mainloop()
