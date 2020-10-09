54ae971a93a7a14f8e9c0621f486fb7079e4caa1
bermby38 <bermby38@hotmail.com>
2020-10-09 11:20:26 +0700

    Create Lecture 80.py

---
+++ Lecture 80.py
@@ -0,0 +1,14 @@
+import sys
+import pa
+
+randomList = [10,'6','a', 0, 2]
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1 / int(entry)
+        break
+    except:
+        print("Oops!", sys.exc_info()[0], "occured.")
+        print("Next ecntry.")
+        print()
+print("The reciprocal of", entry, "is", r)
